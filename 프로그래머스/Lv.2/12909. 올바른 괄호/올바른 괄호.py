918de6a262f9f1057139e8fabebf73603a21ea50
Juno <70925118+qiakzm@users.noreply.github.com>
2024-01-11 19:36:14 +0900

    [level Lv.2] Title: 올바른 괄호, Time: 8.44 ms, Memory: 10.7 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,24 @@
+def solution(s):
+    answer = True
+    s = list(s)
+    stack = []
+    cnt = 0
+    
+    for i in s:
+        if i == '(':
+            cnt += 1
+        else:
+            cnt -= 1
+        if cnt == -1:
+            return False
+    
+    if cnt == 0:
+        if s[0] == ')':
+            return False
+        elif s[-1] == '(':
+            return False
+        return True
+
+    if cnt != 0:
+        return False
+
