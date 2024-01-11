ac49c66e7899d541827a19333d0d387666ff9cab
Juno <70925118+qiakzm@users.noreply.github.com>
2024-01-11 15:10:39 +0900

    [level 2] Title: 전화번호 목록, Time: 188.59 ms, Memory: 34.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -0,0 +1,15 @@
+def solution(phone_book):
+    #1. phone_book 요소 해쉬 1로 지정
+    hashmap = {}
+    for number in phone_book:
+        hashmap[number] = 1
+    
+    #2. 접두어가 해시맵에 존재하는가
+    for phone_number in phone_book:
+        jubdoo = ''
+        for number in phone_number:
+            jubdoo += number
+            #3. 접두어 찾기.(본인 제외)
+            if jubdoo in hashmap and jubdoo != phone_number:
+                return False
+    return True
