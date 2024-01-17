269006493bde18b00e19c3069dc522a0a56db18f
Juno <70925118+qiakzm@users.noreply.github.com>
2024-01-17 20:14:10 +0900

    [level 0] Title: 가위 바위 보, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
@@ -0,0 +1,11 @@
+def solution(rsp):
+    answer = []
+    for i in rsp:
+        if i == '2':
+            answer.append('0')
+        elif i == '0':
+            answer.append('5')
+        elif i == '5':
+            answer.append('2')
+            
+    return ''.join(answer)
