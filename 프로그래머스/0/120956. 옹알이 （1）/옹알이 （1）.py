33615bf4c6aaaf58381de1b71288c55d8e4de7a5
Juno <70925118+qiakzm@users.noreply.github.com>
2024-01-21 11:29:30 +0900

    [level 0] Title: 옹알이 (1), Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.py
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.py
@@ -1,9 +1,9 @@
 def solution(babbling):
-    can = ["aya", "ye", "woo", "ma"]
+    oong = ["aya", "ye", "woo", "ma"]
     for k, i in enumerate(babbling):
-        for j in can:
-            if j*2 in i:
+        for j in oong:
+            if j * 2 in i:
                 break
-            babbling[k]=babbling[k].replace(j, " ")
-        babbling[k]=babbling[k].replace(" ", "")
+            babbling[k] = babbling[k].replace(j, " ")
+        babbling[k] = babbling[k].replace(" ", "")
     return babbling.count("")
